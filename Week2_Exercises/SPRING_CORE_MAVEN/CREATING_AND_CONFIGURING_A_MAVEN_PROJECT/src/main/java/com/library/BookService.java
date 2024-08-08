15dac2ac8c033faf85588788ceb6fa15a797edee
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-08 15:32:52 +0530

    Create BookService.java

---
+++ Week2_Exercises/SPRING_CORE_MAVEN/CREATING_AND_CONFIGURING_A_MAVEN_PROJECT/src/main/java/com/library/BookService.java
@@ -0,0 +1,18 @@
+package com.library;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Setter method for dependency injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Getter method for verification
+    public BookRepository getBookRepository() {
+        return bookRepository;
+    }
+
+    // Other methods and business logic
+}
