7a1f3970544d54d67cbb0df303d343694d50c16b
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-15 16:06:34 +0530

    Create DepartmentService.java

---
+++ Week3_Exercises/Employee_Management_System/src/main/java/com/example/EmployeeManagementSystem/Application/service/DepartmentService.java
@@ -0,0 +1,27 @@
+package com.example.employeemanagementsystem.service;
+
+import com.example.employeemanagementsystem.model.Department;
+import com.example.employeemanagementsystem.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public List<Department> getAllDepartments() {
+        return departmentRepository.findAll();
+    }
+
+    public Department getDepartmentById(Long id) {
+        return departmentRepository.findById(id).orElse(null);
+    }
+
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+}
