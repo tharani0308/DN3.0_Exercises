01f36b71929e112d0b48c0ec16c788de38994ce2
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-15 17:13:54 +0530

    Create data.sql

---
+++ Week3_Exercises/EMS_Implementing_Pagination_And_Sorting/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO employee (name, department, salary) VALUES ('Alice', 'HR', 50000);
+INSERT INTO employee (name, department, salary) VALUES ('Bob', 'IT', 60000);
+INSERT INTO employee (name, department, salary) VALUES ('Charlie', 'IT', 55000);
+INSERT INTO employee (name, department, salary) VALUES ('Diana', 'Finance', 70000);
