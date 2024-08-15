af96d4fc657d2c7dc80bfc75e5004548d91e2c12
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-15 17:56:55 +0530

    Create DataSourceConfig.java

---
+++ Week3_Exercises/EMS_Customizing_Data_Source/src/java/example/employeemanagementsystem/config/DataSourceConfig.java
@@ -0,0 +1,32 @@
+package com.example.employeemanagementsystem.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "primaryDataSource")
+    public DataSource primaryDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/primarydb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    @Bean(name = "secondaryDataSource")
+    public DataSource secondaryDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/secondarydb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+}
