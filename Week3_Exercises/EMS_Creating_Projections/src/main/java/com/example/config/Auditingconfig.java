0acd30722c2ea8031166ca9c3c79b074e75e2e7d
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-15 17:38:47 +0530

    Create Auditingconfig.java

---
+++ Week3_Exercises/EMS_Creating_Projections/src/main/java/com/example/config/Auditingconfig.java
@@ -0,0 +1,10 @@
+package com.example.employeemanagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+    // Configuration for auditing
+}
