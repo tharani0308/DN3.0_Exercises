034d588a79ee56d33558246894ba52cc95c53cf4
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-15 15:50:39 +0530

    Create EmployeeController.java

---
+++ Week3_Exercises/Employee_Management_System/src/main/java/com/example/EmployeeManagementSystem/Application/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.example.employeemanagementsystem.controller;
+
+import com.example.employeemanagementsystem.model.Employee;
+import com.example.employeemanagementsystem.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+}
