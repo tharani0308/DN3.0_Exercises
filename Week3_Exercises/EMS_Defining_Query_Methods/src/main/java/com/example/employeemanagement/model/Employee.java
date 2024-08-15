91385419c6a73ba59ef536739cfc9cdd9792a073
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-15 17:04:20 +0530

    Create Employee.java

---
+++ Week3_Exercises/EMS_Defining_Query_Methods/src/main/java/com/example/employeemanagement/model/Employee.java
@@ -0,0 +1,19 @@
+package com.example.employeemanagement.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String department;
+    private Double salary;
+
+    // Getters and Setters
+}
