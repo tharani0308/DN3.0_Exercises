7b04d6a018248304edeaa4b14bdcba1ebc87de7a
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-15 16:30:49 +0530

    Create EmployeeService.java

---
+++ Week3_Exercises/EMS_Creating_Repositories/src/test/java/com/example/employeemanagementsyste/service/EmployeeService.java
@@ -0,0 +1,35 @@
+package com.example.employeemanagementsystem.service;
+
+import com.example.employeemanagementsystem.model.Employee;
+import com.example.employeemanagementsystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getEmployeesByDepartment(Long departmentId) {
+        return employeeRepository.findByDepartmentId(departmentId);
+    }
+
+    public Employee getEmployeeByEmail(String email) {
+        return employeeRepository.findByEmail(email);
+    }
+}
