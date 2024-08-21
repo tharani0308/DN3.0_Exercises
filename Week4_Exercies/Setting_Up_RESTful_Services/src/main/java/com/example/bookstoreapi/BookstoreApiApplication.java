aa5977c110a041e5182ba63e710985e876aad6d6
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-21 10:23:20 +0530

    Create BookstoreApiApplication.java

---
+++ Week4_Exercies/Setting_Up_RESTful_Services/src/main/java/com/example/bookstoreapi/BookstoreApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.bookstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApiApplication.class, args);
+    }
+}
