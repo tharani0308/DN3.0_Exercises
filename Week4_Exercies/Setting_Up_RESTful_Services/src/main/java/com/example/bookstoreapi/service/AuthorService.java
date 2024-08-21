6672b9c33debeac4fa89dc0497a7079b37f0105b
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-21 10:40:06 +0530

    Create AuthorService.java

---
+++ Week4_Exercies/Setting_Up_RESTful_Services/src/main/java/com/example/bookstoreapi/service/AuthorService.java
@@ -0,0 +1,31 @@
+package com.example.bookstoreapi.service;
+
+import com.example.bookstoreapi.model.Author;
+import com.example.bookstoreapi.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Author getAuthorById(Long id) {
+        return authorRepository.findById(id).orElse(null);
+    }
+
+    public Author saveAuthor(Author author) {
+        return authorRepository.save(author);
+    }
+
+    public void deleteAuthor(Long id) {
+        authorRepository.deleteById(id);
+    }
+}
