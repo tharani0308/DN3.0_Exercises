3c1e9a84b75becd2fd220daccfccfa941faacb0c
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-22 19:47:28 +0530

    Create Customer.java

---
+++ Week4_Exercies/Introduction_to_Data_Transferr_Objects/src/main/java/com/example/bookstore/entity/Customer.java
@@ -0,0 +1,22 @@
+package com.example.bookstore.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String registeredDate;
+
+    // Getters and Setters
+}
