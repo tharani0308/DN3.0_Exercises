55f35bfaf36d9f53da07f3811715441003e7ac82
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-22 19:40:56 +0530

    Create CustomerMapper.java

---
+++ Week4_Exercies/Introduction_to_Data_Transferr_Objects/src/main/java/com/example/bookstore/mapper/CustomerMapper.java
@@ -0,0 +1,16 @@
+package com.example.bookstore.mapper;
+
+import com.example.bookstore.dto.CustomerDTO;
+import com.example.bookstore.entity.Customer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CustomerMapper {
+
+    CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
+
+    CustomerDTO customerToCustomerDTO(Customer customer);
+
+    Customer customerDTOToCustomer(CustomerDTO customerDTO);
+}
