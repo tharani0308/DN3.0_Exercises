e0cc9ad4369ed958e3599159f855f896e70db321
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-22 19:40:03 +0530

    Create CustomerDTO.java

---
+++ Week4_Exercies/Introduction_to_Data_Transferr_Objects/src/main/java/com/example/bookstore/dto/CustomerDTO.java
@@ -0,0 +1,51 @@
+package com.example.bookstore.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CustomerDTO {
+    
+    @JsonProperty("id")
+    private Long id;
+    
+    @JsonProperty("name")
+    private String name;
+    
+    @JsonProperty("email")
+    private String email;
+    
+    @JsonProperty("registeredDate")
+    private String registeredDate;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+}
