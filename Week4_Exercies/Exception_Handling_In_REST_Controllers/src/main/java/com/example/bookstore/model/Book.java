9c783c6995b7ea2faab1442de0ea2dd3035b777c
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-22 19:32:30 +0530

    Create Book.java

---
+++ Week4_Exercies/Exception_Handling_In_REST_Controllers/src/main/java/com/example/bookstore/model/Book.java
@@ -0,0 +1,9 @@
+package com.example.bookstore.model;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+
+    // Constructors, getters, and setters
+}
