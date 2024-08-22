30d0ef1d9dbcd783869ac9ff4effdd7765c978c4
THARANI R <104920843+tharani0308@users.noreply.github.com>
2024-08-22 19:00:09 +0530

    Create BookController.java

---
+++ Week4_Exercies/Customizing_Response_Status_And_Headers/src/main/java/com/example/bookstore/controller/BookController.java
@@ -0,0 +1,60 @@
+package com.example.bookstore.controller;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.service.BookService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBook(@PathVariable Long id) {
+        Book book = bookService.getBook(id);
+        if (book != null) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Custom-Header", "CustomHeaderValue");
+            return new ResponseEntity<>(book, headers, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        Book createdBook = bookService.addBook(book);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/books/" + createdBook.getId());
+        return new ResponseEntity<>(createdBook, headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        Book updatedBook = bookService.updateBook(id, book);
+        if (updatedBook != null) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Custom-Update-Header", "UpdateSuccessful");
+            return new ResponseEntity<>(updatedBook, headers, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        if (bookService.deleteBook(id)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
